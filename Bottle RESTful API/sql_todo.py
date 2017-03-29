4f55e9c6e34ce4968515d3c63ad89add9cd7f50d
KaiyuanZeng <zkynz94@gmail.com>
2017-03-30 03:24:57 +1300

    Delete sql_todo.py

--- Bottle RESTful API/sql_todo.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect('todo.db')
-con.execute("CREATE TABLE todo (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)")
-con.execute("INSERT INTO todo (task,status) VALUES ('Upskill Bottle',0)")
-con.execute("INSERT INTO todo (task,status) VALUES ('Upskill Flask',1)")
-con.execute("INSERT INTO todo (task,status) VALUES ('Upskill Docker',1)")
-con.commit()
