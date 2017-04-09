78cc53b3e2306fc1075b7c7c3e272ccb6d6a7115
KaiyuanZeng <zkynz94@gmail.com>
2017-04-10 01:54:04 +1200

    Kaiyuan Zeng

--- Bottle RESTful API/bottledocCon.py
+++ Bottle RESTful API/bottledocCon.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run
+from bottle import route,run, request,get,post, delete
 
 @route('/todo')
 def todo_list():
@@ -7,6 +7,22 @@ def todo_list():
     c = conn.cursor()
     c.execute("SELECT * FROM User")
     result = c.fetchall()
+    c.close()
     return str(result)
+
+@get('/todo/<id>')
+def getOne(id):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("Select * from User where User_ID = (?)", (id))
+    result = "User Name: "+ c.fetchall()
+    c.execute("Select * from Todo where UserID = (?)", (id))
+    result += "Todo Detail: " + c.fetchall()
+    c.close()
+    return str(result)
+	
+
+#@route('/new', method = 'GET')
+
 
 run(debug=True, reloader=True)
