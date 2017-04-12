a9ce1c130dceac4f01ff733377b2d7c1e964671d
muba <mubasher_k@hotmail.com>
2017-04-12 16:48:56 +1200

    Changed bottle code Signed-off-by: muba <mubasher_k@hotmail.com>

--- Bottle RESTful API/Restful.py
+++ Bottle RESTful API/Restful.py
@@ -1,32 +1,42 @@
-import bottle
 import sqlite3
 from bottle import route,run, request,get,post, delete, error
 
 @get('/todo')
 def todo_list():
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect('Practice.db')
     c = conn.cursor()
     c.execute("SELECT * FROM User")
-    result = c.fetchall()
+    result = []
+    result.append({'User table': c.fetchall()})
     c.execute("SELECT * FROM Todo")
-    result += c.fetchall()
+    result.append({'Todo table': c.fetchall()})
     c.close()
     return str(result)
 
 @get('/todo/<id>')
 def getOne(id):
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect('Practice.db')
     c = conn.cursor()
     c.execute("Select * from User where User_ID = (?)", (id))
-    result =  c.fetchall()
+    result = []
+    result.append({'User table': c.fetchall()})
     c.execute("Select * from Todo where UserID = (?)", (id))
-    result +=  c.fetchall()
+    result.append({'Todo table': c.fetchall()})
     c.close()
     return str(result)
 
+@post('/animal')
+def addOne():
+    conn = sqlite3.connect('Practice.db')
+    c = conn.cursor()
+	#new_animal = {'UserID' : request.json.get('UserID'), 'details' : request.json.get('Details')}
+	#animals.append(new_animal)
+	#return {'animals' : animals}
+    return''
+
 @delete('/todo/delete/<id>')
 def removeOne(id):
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect('Practice.db')
     c = conn.cursor()
     c.execute("Delete from Todo where todo_ID = (?)", (id))
     conn.commit()
@@ -41,9 +51,6 @@ def mistake403(code):
 def mistake404(code):
     return 'Sorry, this page does not exist!'
 
-if __name__ == '__main__':
-	bottle.debug(True)
-	bottle.run(reloader= True)
-else:
-	application = bottle.default_app()
-	
+
+
+run(debug=True, reloader=True)
