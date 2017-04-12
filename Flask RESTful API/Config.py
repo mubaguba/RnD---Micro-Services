4db8b0e620e01d5ce29b973f5e93e294399c9d13
muba <mubasher_k@hotmail.com>
2017-04-12 16:59:43 +1200

    edited config Signed-off-by: muba <mubasher_k@hotmail.com>

--- Flask RESTful API/Config.py
+++ Flask RESTful API/Config.py
@@ -1,6 +1,6 @@
 #All of this is for the location of the database file.
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'Practise.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'Practice.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 print (basedir) #Prints the path.
